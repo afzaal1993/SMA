88e0cfcb3a6ea7839ad0e42939208429b273a813
Moahmmed A. Faroq <mafzaal@taqnia.com>
2024-09-18 09:45:44 +0300

    updates

--- Backend/SMA.API/DTOs/RegisterDto.cs
+++ Backend/SMA.API/DTOs/RegisterDto.cs
@@ -1,8 +1,13 @@
-﻿    namespace SMA.API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SMA.API.DTOs
 {
     public class RegisterDto
     {
-        public required string Username { get; set; }
-        public required string Password { get; set; }
+        [Required]
+        public string Username { get; set; } = string.Empty;
+        [Required]
+        [StringLength(8, MinimumLength = 4)]
+        public string Password { get; set; } = string.Empty;
     }
 }
--- Backend/SMA.API/Program.cs
+++ Backend/SMA.API/Program.cs
@@ -1,4 +1,5 @@
 using SMA.API.Extensions;
+using SMA.API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,9 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseMiddleware<ExceptionMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
